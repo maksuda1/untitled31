9094e11c3992e3174fde25ede438fead4286afb4
maksudahan <maksuda.2289@gmail.com>
2022-04-26 16:07:15 +0300

    commit2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,14 +7,14 @@ public class Main {
     public static void main(String[] args) {
 
 
-    }  static  int getSum(int ... a){
+    }  static  int getSum(int ... a) {
         int sum = 0;
-        for(int result : a){
-    sum+=result;
+        for (int result : a) {
+            sum += result;
 
 
-
-    }return sum;
+        }
+    }
 
 
 
